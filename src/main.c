78f8543890920fda97c33e998797a14b6587561c
DavidKendall <david.kendall@northumbria.ac.uk>
2015-08-28 14:27:01 +0100

    uCOS-II example for LPC4088 - Initial commit

---
+++ src/main.c
@@ -0,0 +1,108 @@
+#include <stdbool.h>
+#include <ucos_ii.h>
+#include "gpioPin.h"
+
+/*
+*********************************************************************************************************
+*                                            APPLICATION TASK PRIORITIES
+*********************************************************************************************************
+*/
+
+typedef enum {
+  APP_TASK_LED1_PRIO = 4,
+  APP_TASK_LED2_PRIO
+} taskPriorities_t;
+
+/*
+*********************************************************************************************************
+*                                            APPLICATION TASK STACKS
+*********************************************************************************************************
+*/
+
+#define  APP_TASK_LED1_STK_SIZE              256
+#define  APP_TASK_LED2_STK_SIZE              256
+
+static OS_STK appTaskLED1Stk[APP_TASK_LED1_STK_SIZE];
+static OS_STK appTaskLED2Stk[APP_TASK_LED2_STK_SIZE];
+
+/*
+*********************************************************************************************************
+*                                            APPLICATION FUNCTION PROTOTYPES
+*********************************************************************************************************
+*/
+
+static void appTaskLED1Led(void *pdata);
+static void appTaskLED2Led(void *pdata);
+
+/*
+*********************************************************************************************************
+*                                            GLOBAL TYPES AND VARIABLES 
+*********************************************************************************************************
+*/
+
+typedef enum {
+	LED1 = 0,
+	LED2
+} deviceNames_t;
+
+gpioPin_t pin[2];
+
+/*
+*********************************************************************************************************
+*                                            GLOBAL FUNCTION DEFINITIONS
+*********************************************************************************************************
+*/
+
+int main() {
+
+  /* Initialise the GPIO pins */
+	gpioPinInit(&pin[LED1], 1, 18, OUTPUT_PIN);
+	gpioPinInit(&pin[LED2], 0, 13, OUTPUT_PIN);
+
+  /* Initialise the OS */
+  OSInit();                                                   
+
+  /* Create the tasks */
+  OSTaskCreate(appTaskLED1Led,                               
+               (void *)0,
+               (OS_STK *)&appTaskLED1Stk[APP_TASK_LED1_STK_SIZE - 1],
+               APP_TASK_LED1_PRIO);
+  
+  OSTaskCreate(appTaskLED2Led,                               
+               (void *)0,
+               (OS_STK *)&appTaskLED2Stk[APP_TASK_LED2_STK_SIZE - 1],
+               APP_TASK_LED2_PRIO);
+
+  
+  /* Start the OS */
+  OSStart();                                                  
+  
+  /* Should never arrive here */ 
+  return 0;      
+}
+
+/*
+*********************************************************************************************************
+*                                            APPLICATION TASK DEFINITIONS
+*********************************************************************************************************
+*/
+
+static void appTaskLED1Led(void *pdata) {
+  /* Start the OS ticker -- must be done in the highest priority task */
+  SysTick_Config(SystemCoreClock / OS_TICKS_PER_SEC);
+  
+  /* Task main loop */
+  while (true) {
+    gpioPinToggle(&pin[LED1]);
+    OSTimeDlyHMSM(0,0,0,500);
+  }
+}
+
+static void appTaskLED2Led(void *pdata) {
+  while (true) {
+    OSTimeDlyHMSM(0,0,0,500);
+    gpioPinToggle(&pin[LED2]);
+  } 
+}
+
+
